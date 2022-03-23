8d0a8a1c939f5feb4ebffb3a3f5f4d7f97509779
Andres Arevalo <andresfelipear@gmail.com>
2022-03-23 09:57:08 -0700

    removed unused data, working in navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
 import Footer from './components/footer/Footer'
+import Header from './components/header/Header';
 import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path='/signup' element={<SignUpPage/>} />
         <Route path='/login' element={<LoginPage/>} />
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,6 +7,42 @@ function Header() {
 
     return (
         <div>
+            <Navbar>
+                <Navbar.Brand>
+                    <Navbar.Item href="/">
+                        <img src="/logo.png" alt="logo" />
+                    </Navbar.Item>
+
+                </Navbar.Brand>
+                <Navbar.Menu>
+                    <Navbar.Container>
+                        <Navbar.Item active={false} hoverable={true} >
+                            <Navbar.Link>
+                                More   
+                            </Navbar.Link>
+                            <Navbar.Dropdown>
+                                <Navbar.Item>
+                                    Breakfasts
+                                </Navbar.Item>
+                                <Navbar.Item>
+                                    Delivery Policy
+                                </Navbar.Item>
+                                <Navbar.Item>
+                                    Contact
+                                </Navbar.Item>
+                            </Navbar.Dropdown>
+                        </Navbar.Item>
+                    </Navbar.Container>
+                    <Navbar.Container align="right">
+                        <Navbar.Item href="/login">
+                            Login
+                        </Navbar.Item>
+                        <Navbar.Item href="/signup">
+                            SignUp
+                        </Navbar.Item>
+                    </Navbar.Container>
+                </Navbar.Menu>
+            </Navbar>
 
         </div>
     )
