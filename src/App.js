5695271936af4d46842e5b1c8c368a1084518e10
Andres Arevalo <andresfelipear@gmail.com>
2022-03-22 18:17:25 -0700

    sign up form: select avatar done!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import SignUpPage from './pages/SignUpPage';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      <>Hola Mundo</>
       <Routes>
-        <Route path='/signup' element={SignUpPage}/>
+        <Route path='/signup' element={<SignUpPage/>} />
       </Routes>
     </div>
   );
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,14 +1,64 @@
 import React from 'react'
-import {Section, Container, Form} from "react-bulma-components"
+import { useState, useEffect } from 'react'
+import { Section, Container, Form, Icon, Select } from "react-bulma-components"
 function SignUpPage() {
-  return (
-    <Section mt={6} >
-        <Container backgroundColor='light' className='widthForm'>
+    //values fontawesome icons avatar
+    const iconTie = "fa-user-tie";
+    const iconNinja = "fa-user-ninja";
+    const iconPlus = "fa-user-plus";
+    const iconAstronaut = "fa-user-astronaut";
 
-        </Container>
+    const [selectAvatar, setSelectAvatar] = useState("")
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [icon, setIcon] = useState(iconTie);
 
-    </Section>
-  )
+    useEffect(() => {
+        if (selectAvatar) {
+            if (selectAvatar === "avatar 1") {
+                setIcon(iconTie)
+            } else if (selectAvatar === "avatar 2") {
+                setIcon(iconNinja)
+            }
+            else if (selectAvatar === "avatar 3") {
+                setIcon(iconPlus)
+            }
+            else if (selectAvatar === "avatar 4") {
+                setIcon(iconAstronaut)
+            }
+        }
+    }, [selectAvatar])
+    return (
+        <Section mt={6} >
+            <Container >
+                <form className='formWidth'>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Field>
+                        <Form.Control>
+
+                            <Form.Select onChange={(e) => setSelectAvatar(e.target.value)}>
+                                <option value="avatar 1">#1</option>
+                                <option value="avatar 2">#2</option>
+                                <option value="avatar 3">#3</option>
+                                <option value="avatar 4">#4</option>
+                            </Form.Select>
+                            <Icon align='left' size="large">
+                                <i className={`fas fa-user`}></i>
+                            </Icon>
+                        </Form.Control>
+
+                    </Form.Field>
+
+
+
+                </form>
+
+            </Container>
+
+        </Section>
+    )
 }
 
 export default SignUpPage
