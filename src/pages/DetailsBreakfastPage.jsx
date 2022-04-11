7fe3f3c25012d0de25e67604f89b2d2b515dbc28
Andres Arevalo <andresfelipear@gmail.com>
2022-04-11 16:07:19 -0700

    working on styling detailsBreakfast

--- src/pages/DetailsBreakfastPage.jsx
+++ src/pages/DetailsBreakfastPage.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useCallback, useEffect } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import { Notification, Heading, Section, Box, Image, Icon } from "react-bulma-components"
+import { Notification, Heading, Section, Box, Image, Icon, Form, Button } from "react-bulma-components"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { faStar } from "@fortawesome/free-regular-svg-icons"
 import Modal from '../components/notification/Modal';
 
 function DetailsBreakfastPage() {
@@ -13,6 +13,7 @@ function DetailsBreakfastPage() {
     const [loading, setLoading] = useState(false)
     const [notiTitle, setNotiTitle] = useState("")
     const [notiBody, setNotiBody] = useState("")
+    const [quantity, setQuantity] = useState(0)
 
     const fetchBreakfast = useCallback(() => {
         setLoading(true);
@@ -42,13 +43,6 @@ function DetailsBreakfastPage() {
         }
     }, [breakfast]);
 
-    useEffect(() => {
-        if (breakfast) {
-            const array = breakfast.Items[0];
-            console.log(array)
-        }
-
-    }, [breakfast])
 
     const closeModal = () => {
         const modalContainer = document.getElementById("modal-container");
@@ -62,11 +56,6 @@ function DetailsBreakfastPage() {
         setNotiBody(message);
     }
 
-    // const joinValuesObject = (obj) => {
-    //     let stringObject = "";
-    //     for()
-
-    // }
 
     if (loading) {
         return (
@@ -79,24 +68,39 @@ function DetailsBreakfastPage() {
     return (
         <Section>
             {breakfast && (
-                <Box>
-                    <div>
-                        <Heading>BREAKFAST - {breakfast.Name}</Heading>
+                <Box display='flex' style={{ alignItems: "center", justifyContent: "space-evenly" }}>
+                    <Image src={`/images/breakfasts/${breakfast.Img}.${process.env.REACT_APP_API_FORMAT_IMAGES}`} />
+                    <div style={{ paddingLeft: "20px" }}>
+                        <Heading textTransform='uppercase'>{breakfast.Name}</Heading>
                         <div>
                             <Heading subtitle>Details: </Heading>
                             <ul>
                                 {breakfast.Items.map((item) => {
-                                    const stringObject = Object.values(item).map((val)=> {return val}).join("");
+                                    const stringObject = Object.values(item).map((val) => { return val }).join("");
                                     return (
-                                        <li style={{display:"flex", alignItems:"center"}}>
-                                            <Icon mr={2} size="medium" style={{ color: "#905960" }}>
-                                                <FontAwesomeIcon size='lg' icon={faStar} />
+                                        <li style={{ display: "flex", alignItems: "center" }}>
+                                            <Icon mr={2} size="small" style={{ color: "#905960" }}>
+                                                <FontAwesomeIcon size='small' icon={faStar} />
                                             </Icon>
                                             <span>{stringObject}</span>
                                         </li>
                                     )
                                 })}
                             </ul>
+                            <Heading my={4} subtitle>This price don't include the cost of delivery</Heading>
+                            <Heading size={3} subtitle weight='semibold' style={{ fontStyle: "italic" }}>${breakfast.Price.toFixed(2)}</Heading>
+                            <Form.Field display='flex' alignItems='center'>
+                                <Form.Label mr={2}>Quantity</Form.Label>
+                                <Form.Select size={'small'} defaultValue={1} value={quantity} onChange={(e) => { setQuantity(e.target.value) }}>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                </Form.Select>
+                            </Form.Field>
+
+                            <Button style={{color:"#905960", borderColor:"#905960", fontWeight:"500"}}>ADD TO THE CART</Button>
+
                         </div>
                     </div>
                 </Box>
