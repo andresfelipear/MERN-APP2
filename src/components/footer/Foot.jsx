aaf72bc5fbc89030abe45bc03d9ce226136ddc31
Andres Arevalo <andresfelipear@gmail.com>
2022-04-28 03:00:13 -0700

    fixed some errors

--- src/components/footer/Foot.jsx
+++ src/components/footer/Foot.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Footer, Breadcrumb, Box, Icon, Image, Heading } from "react-bulma-components"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faFacebook, faInstagram} from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons"
 
 import "./Foot.css"
 function Foot() {
     return (
         <Footer style={{ paddingInline: "0px" }} textAlign="center" backgroundColor='white'>
-            <Box shadowless radiusless backgroundColor='danger-light' marginless style={{paddingBottom:0}}>
+            <Box shadowless radiusless backgroundColor='danger-light' marginless style={{ paddingBottom: 0 }}>
                 <Heading marginless textAlign="center"> Our Payment Methods</Heading>
-                <img src='/images/medios-de-pago.png'/>
+                <img src='/images/medios-de-pago.png' alt='payment methods' />
             </Box>
             <Box radiusless display="flex" shadowless marginless justifyContent="center" style={{ backgroundColor: "#905960" }}>
                 <Breadcrumb separator="arrow">
-                    <Breadcrumb.Item active>
-                        <a style={{ color: "white", flexDirection: "column", marginInline: "10px" }}>
+                    <Breadcrumb.Item >
+                        <div style={{ color: "white", flexDirection: "column", marginInline: "10px" }}>
                             <strong>Account Details</strong>
                             <div>
                                 <Icon size="large" style={{ marginLeft: "0px" }}>
@@ -23,11 +23,11 @@ function Foot() {
                                 +1 (236)-986-3592
                             </div>
 
-                        </a>
+                        </div>
                     </Breadcrumb.Item>
 
-                    <Breadcrumb.Item active>
-                        <a style={{ color: "white", flexDirection: "column", marginInline: "10px" }}>
+                    <Breadcrumb.Item >
+                        <div style={{ color: "white", flexDirection: "column", marginInline: "10px" }}>
                             <strong> Contact Us</strong>
                             <div style={{ flexDirection: "row", display: "flex", alignItems: "center" }}>
                                 <Icon size="large">
@@ -39,27 +39,32 @@ function Foot() {
                                 </div>
                             </div>
 
-                        </a>
+                        </div>
                     </Breadcrumb.Item>
 
-                    <Breadcrumb.Item active>
-                        <a style={{ color: "white", flexDirection: "column", marginInline: "10px" }}>
-                            <strong> Coverage Map</strong>
+                    <Breadcrumb.Item >
+                        <div style={{ color: "white", flexDirection: "column", marginInline: "10px" }}>
+                            <strong >  Coverage Map</strong>
                             <div style={{ marginTop: "10px" }}>
                                 <Image src='https://bulma.io/images/placeholders/128x128.png' />
                             </div>
-                        </a>
-                        <a style={{ color: "white", flexDirection: "column", marginInline: "10px" }}>
-                            <strong> Social Media</strong>
-                            <div style={{ marginTop: "10px" }}>
-                                <Icon size="large">
-                                    <FontAwesomeIcon size="lg" icon={faFacebook}/>
-                                </Icon>
-                                <Icon size="large">
-                                    <FontAwesomeIcon size="lg" icon={faInstagram}/>
-                                </Icon>
+                        </div>
+                        <div style={{ color: "white", flexDirection: "column", marginInline: "10px" }}>
+                            <strong > Social Media</strong>
+                            <div style={{ marginTop: "10px", display:'flex'}}>
+                                <a href={'https://www.facebook.com/Desayunosbogota22'} target="_blank">
+                                    <Icon size="large">
+                                        <FontAwesomeIcon size="lg" icon={faFacebook} />
+                                    </Icon>
+                                </a>
+                                <a href='https://www.instagram.com/desayunosbogota22' target="_blank">
+                                    <Icon size="large">
+                                        <FontAwesomeIcon size="lg" icon={faInstagram} />
+                                    </Icon>
+                                </a>
+
                             </div>
-                        </a>
+                        </div>
                     </Breadcrumb.Item>
                 </Breadcrumb>
             </Box>
